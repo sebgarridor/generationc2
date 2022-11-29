24243be608b87b9c14416bc3e0b6f94a6acffb1d
sebgarridor <sebgarridor@gmail.com>
2022-11-28 22:57:16 -0300

    Bases de html

--- JavaScript/SQL/Script.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO generationc2.estudiante (nombre,id,apellido) 
-VALUES ('Silvio',7,'Dante'); 
-INSERT INTO generationc2.estudiante (nombre,id,apellido) 
-VALUES ('Adriana',8,'La Cerva'); 
-INSERT INTO generationc2.estudiante (nombre,id,apellido) 
-VALUES ('Meadow',9,'Soprano'); 
